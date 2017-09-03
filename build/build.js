5156a2252db1a215a415908436089395580066c2
dave <18234172886@163.com>
2017-09-03 14:43:05 +0800

    Change build.js es5 => es6

--- build/build.js
+++ build/build.js
@@ -1,40 +1,45 @@
-require('./check-versions')()
+/**
+ * 正式环境打包node脚本
+ * created by dave 2017/09/03
+ *
+ */
+require('./check-versions')();
+process.env.NODE_ENV = 'production';
 
-process.env.NODE_ENV = 'production'
+import ora from 'ora';      // 终端进度转轮
+import rm from 'rimraf';    // node unix命令 等同于rm -rf
+import path from 'path';    // node path模块 解析路径
+import chalk from 'chalk';  // 改变命令行中字体颜色
+import webpack from 'webpack';  // 打包工具
+import config from '../config';  // 打包工具配置文件
+import webpackConfig from './webpack.prod.conf';  // 打包工具生产环境配置文件
 
-var ora = require('ora')
-var rm = require('rimraf')
-var path = require('path')
-var chalk = require('chalk')
-var webpack = require('webpack')
-var config = require('../config')
-var webpackConfig = require('./webpack.prod.conf')
+const spinner = ora('building for production...'); // 进度提示文字
+spinner.start();
 
-var spinner = ora('building for production...')
-spinner.start()
-
+// 旧版本静态文件的清除
 rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
-  if (err) throw err
+  if (err) throw err;
   webpack(webpackConfig, function (err, stats) {
-    spinner.stop()
-    if (err) throw err
+    spinner.stop();
+    if (err) throw err;
     process.stdout.write(stats.toString({
       colors: true,
       modules: false,
       children: false,
       chunks: false,
       chunkModules: false
-    }) + '\n\n')
+    }) + '\n\n');
 
     if (stats.hasErrors()) {
-      console.log(chalk.red('  Build failed with errors.\n'))
+      console.log(chalk.red('  Build failed with errors.\n'));
       process.exit(1)
     }
 
-    console.log(chalk.cyan('  Build complete.\n'))
+    console.log(chalk.cyan('  Build complete.\n'));
     console.log(chalk.yellow(
       '  Tip: built files are meant to be served over an HTTP server.\n' +
       '  Opening index.html over file:// won\'t work.\n'
     ))
   })
-})
+});
