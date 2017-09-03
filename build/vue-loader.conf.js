eea38a3c15fb87c3b34ff7571fc121057f96cfe2
dave <18234172886@163.com>
2017-09-03 21:50:13 +0800

    Add doc

--- build/vue-loader.conf.js
+++ build/vue-loader.conf.js
@@ -1,3 +1,7 @@
+/**
+ * Created by dave 2017/09/03
+ * 对于加载的一些资源的设置
+ */
 const utils = require('./utils');
 const config = require('../config');
 const isProduction = process.env.NODE_ENV === 'production';
