15746b191bfadfd376e816eae9721783c03fc55a
dave <18234172886@163.com>
2017-09-03 21:59:57 +0800

    Add file doc and delete useless code

--- config/index.js
+++ config/index.js
@@ -1,5 +1,8 @@
-// see http://vuejs-templates.github.io/webpack for documentation.
-var path = require('path')
+/**
+ * Created by dave 2017/09/03
+ * webpack设置模板
+ */
+const path = require('path');
 
 module.exports = {
   build: {
@@ -8,18 +11,10 @@ module.exports = {
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    productionSourceMap: true,
-    // Gzip off by default as many popular static hosts such as
-    // Surge or Netlify already gzip all static assets for you.
-    // Before setting to `true`, make sure to:
-    // npm install --save-dev compression-webpack-plugin
-    productionGzip: false,
+    productionSourceMap: true, // 代码源文件追踪
+    productionGzip: false, // 压缩包
     productionGzipExtensions: ['js', 'css'],
-    // Run the build command with an extra argument to
-    // View the bundle analyzer report after build finishes:
-    // `npm run build --report`
-    // Set to `true` or `false` to always turn it on or off
-    bundleAnalyzerReport: process.env.npm_config_report
+    bundleAnalyzerReport: process.env.npm_config_report // 构建报告
   },
   dev: {
     env: require('./dev.env'),
@@ -28,11 +23,6 @@ module.exports = {
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
     proxyTable: {},
-    // CSS Sourcemaps off by default because relative paths are "buggy"
-    // with this option, according to the CSS-Loader README
-    // (https://github.com/webpack/css-loader#sourcemaps)
-    // In our experience, they generally work as expected,
-    // just be aware of this issue when enabling this option.
     cssSourceMap: false
   }
-}
+};
--- config/test.env.js
+++ config/test.env.js
@@ -1,6 +1,10 @@
-var merge = require('webpack-merge')
-var devEnv = require('./dev.env')
+/**
+ * Created by dave 2017/09/03
+ * 测试环境 变量
+ */
+const merge = require('webpack-merge');
+const devEnv = require('./dev.env');
 
 module.exports = merge(devEnv, {
   NODE_ENV: '"testing"'
-})
+});
